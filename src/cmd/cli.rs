c106900100a0e026a9104c8f40ac3c756cfb5536
kedap <kedap.dev@protonmail.com>
2021-09-14 18:36:39 -0500

    fix the majority clippy errors and apply rustfmt

--- src/cmd/cli.rs
+++ src/cmd/cli.rs
@@ -9,7 +9,7 @@ use crate::utils::git::parse_url;
 pub fn parse<'a>(matches: &'a ArgMatches<'a>) -> Cmd<'a> {
     if let Some(matches) = matches.subcommand_matches("clone") {
         let args = matches.values_of("args").unwrap().collect::<Vec<_>>();
-        let url = args.get(0).map(|v| *v).unwrap();
+        let url = args.get(0).copied().unwrap();
         let rx = Regex::new(r#"((utils@|http(s)?://)(?P<host>[\w.@]+)([/:]))(?P<owner>[\w,\-_]+)/(?P<repo>[\w,\-_]+)(.utils)?((/)?)"#).unwrap();
         if rx.is_match(url.as_ref()) {
             let clone = parse_url(url, rx);
@@ -17,8 +17,8 @@ pub fn parse<'a>(matches: &'a ArgMatches<'a>) -> Cmd<'a> {
         } else {
             Cmd::Clone(Clone::new(
                 Host::from(url),
-                args.get(1).map(|v| *v),
-                args.get(2).map(|v| *v),
+                args.get(1).copied(),
+                args.get(2).copied(),
             ))
         }
     } else if let Some(open) = matches.subcommand_matches("open") {
@@ -32,20 +32,32 @@ pub fn parse<'a>(matches: &'a ArgMatches<'a>) -> Cmd<'a> {
                 .choices(vec!["Vim", "Nano", "VSCode"])
                 .build();
             println!("{:#?}", requestty::prompt_one(question));
-            Cmd::Config(AppOptions::new("".into(), "".into(), Editor::new(EditorApp::from('v'))))
+            Cmd::Config(AppOptions::new(
+                "".into(),
+                "".into(),
+                Editor::new(EditorApp::from('v')),
+            ))
         } else if config.is_present("owner") {
             let question = requestty::Question::input("owner")
                 .message("What's your Git username:")
                 .build();
             println!("{:#?}", requestty::prompt_one(question));
-            Cmd::Config(AppOptions::new("".into(), "".into(), Editor::new(EditorApp::from('v'))))
+            Cmd::Config(AppOptions::new(
+                "".into(),
+                "".into(),
+                Editor::new(EditorApp::from('v')),
+            ))
         } else if config.is_present("host") {
             let question = requestty::Question::select("host")
                 .message("Choose your Git host:")
                 .choices(vec!["GitHub", "GitLab"])
                 .build();
             println!("{:#?}", requestty::prompt_one(question));
-            Cmd::Config(AppOptions::new("".into(), "".into(), Editor::new(EditorApp::Vim)))
+            Cmd::Config(AppOptions::new(
+                "".into(),
+                "".into(),
+                Editor::new(EditorApp::Vim),
+            ))
         } else {
             let questions = vec![
                 requestty::Question::select("editor")
@@ -61,7 +73,11 @@ pub fn parse<'a>(matches: &'a ArgMatches<'a>) -> Cmd<'a> {
                     .build(),
             ];
             println!("{:#?}", requestty::prompt(questions));
-            Cmd::Config(AppOptions::new("".into(), "".into(), Editor::new(EditorApp::Vim)))
+            Cmd::Config(AppOptions::new(
+                "".into(),
+                "".into(),
+                Editor::new(EditorApp::Vim),
+            ))
         }
     } else {
         Cmd::None
