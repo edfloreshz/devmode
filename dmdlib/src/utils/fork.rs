7acbe2f375a9dd034495e97c83b1b23172333deb
kedap <kedap.dev@protonmail.com>
2021-10-23 23:37:32 -0500

    adding upstream how remote

---
+++ dmdlib/src/utils/fork.rs
@@ -0,0 +1,77 @@
+use anyhow::{Context, Result};
+use git2::Repository;
+use regex::bytes::Regex;
+
+use crate::home;
+use crate::utils::constants::messages::*;
+use crate::utils::host::Host;
+
+pub struct Fork {
+    pub host: Host,
+    pub upstream: Host,
+    pub owner: String,
+    pub repo: String,
+}
+
+impl Default for Fork {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl Fork {
+    pub fn new() -> Self {
+        Self {
+            host: Host::None,
+            upstream: Host::None,
+            owner: "".to_string(),
+            repo: "".to_string(),
+        }
+    }
+    pub fn from(host: Host, upstream: Host, owner: String, repo: String) -> Self {
+        Self {
+            host,
+            upstream,
+            owner,
+            repo,
+        }
+    }
+    pub fn url(&self) -> String {
+        format!("{}/{}/{}", self.host.url(), self.owner, self.repo)
+    }
+
+    pub fn clone_repo(&self) -> Result<()> {
+        let path = format!(
+            "{}/Developer/{}/{}/{}",
+            home().display(),
+            self.host,
+            self.owner,
+            self.repo
+        );
+        println!("Cloning {}/{} from {}...", self.owner, self.repo, self.host);
+        Repository::clone(self.url().as_str(), &path).with_context(|| FAILED_TO_CLONE_REPO)?;
+        Ok(())
+    }
+    pub fn parse_url(url: &str, rx: Regex) -> Result<Clone> {
+        let captures = rx.captures(url.as_ref()).unwrap();
+        let host = captures
+            .get(4)
+            .map(|m| std::str::from_utf8(m.as_bytes()).unwrap())
+            .with_context(|| UNABLE_TO_MAP_URL)?;
+        let owner = captures
+            .get(6)
+            .map(|m| String::from_utf8(Vec::from(m.as_bytes())).unwrap())
+            .with_context(|| UNABLE_TO_MAP_URL)?;
+        let repo = captures
+            .get(7)
+            .map(|m| String::from_utf8(Vec::from(m.as_bytes())).unwrap())
+            .with_context(|| UNABLE_TO_MAP_URL)?;
+        Ok(Clone::from(Host::from(host.into()), owner, repo))
+    }
+    pub fn set_upstream(&self) -> Result<()> {
+        println!("Setting {} how upstream...", self.upstream);
+        Repository::remote("upstream", self.upstream.as_str())
+            .with_context(|| FAILED_TO_SET_REMOTE)?;
+        Ok(())
+    }
+}
