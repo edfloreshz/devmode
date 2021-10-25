c1261fb66c1330f84f31f7fcbd87d35703e419b5
kedap <kedap.dev@protonmail.com>
2021-10-25 11:05:09 -0500

    Adding new values for struct fork

--- dmdlib/src/utils/fork.rs
+++ dmdlib/src/utils/fork.rs
@@ -9,9 +9,11 @@ use crate::utils::host::Host;
 
 pub struct Fork {
     pub host: Host,
-    pub upstream: Host,
+    pub host_upstream: Host,
+    pub owner_upstream: String,
     pub owner: String,
     pub repo: String,
+    pub repo_path: String,
 }
 
 impl Default for Fork {
@@ -24,24 +26,42 @@ impl Fork {
     pub fn new() -> Self {
         Self {
             host: Host::None,
-            upstream: Host::None,
+            host_upstream: Host::None,
+            owner_upstream: "".to_string(),
             owner: "".to_string(),
             repo: "".to_string(),
+            repo_path: "".to_string(),
         }
     }
-    pub fn from(host: Host, upstream: Host, owner: String, repo: String) -> Self {
+    pub fn from(
+        host: Host,
+        host_upstream: Host,
+        owner_upstream: String,
+        owner: String,
+        repo: String,
+    ) -> Self {
         Self {
             host,
-            upstream,
+            host_upstream,
+            owner_upstream,
             owner,
             repo,
+            repo_path: String::new(),
         }
     }
     pub fn url(&self) -> String {
         format!("{}/{}/{}", self.host.url(), self.owner, self.repo)
     }
+    pub fn url_upstream(&self) -> String {
+        format!(
+            "{}/{}/{}",
+            self.host_upstream.url(),
+            self.owner_upstream,
+            self.repo
+        )
+    }
 
-    pub fn clone_repo(&self) -> Result<()> {
+    pub fn clone_repo(&mut self) -> Result<()> {
         let path = format!(
             "{}/Developer/{}/{}/{}",
             home().display(),
@@ -51,6 +71,7 @@ impl Fork {
         );
         println!("Cloning {}/{} from {}...", self.owner, self.repo, self.host);
         Repository::clone(self.url().as_str(), &path).with_context(|| FAILED_TO_CLONE_REPO)?;
+        self.repo_path = path;
         Ok(())
     }
     pub fn parse_url(url: &str, rx: Regex) -> Result<Self> {
@@ -67,6 +88,10 @@ impl Fork {
             .get(6)
             .map(|m| String::from_utf8(Vec::from(m.as_bytes())).unwrap())
             .with_context(|| UNABLE_TO_MAP_URL)?;
+        let owner_upstream = captures
+            .get(6)
+            .map(|m| String::from_utf8(Vec::from(m.as_bytes())).unwrap())
+            .with_context(|| UNABLE_TO_MAP_URL)?;
         let repo = captures
             .get(7)
             .map(|m| String::from_utf8(Vec::from(m.as_bytes())).unwrap())
@@ -75,15 +100,19 @@ impl Fork {
             Host::from(host.into()),
             Host::from(upstream.into()),
             owner,
+            owner_upstream,
             repo,
         ))
     }
 
-    pub fn set_upstream(&self, path_project: String) -> Result<()> {
-        println!("Setting {} how upstream...", self.upstream);
-        let project = Repository::open(Path::new(&path_project)).expect(NO_PROJECT_FOUND);
+    pub fn set_upstream(&self) -> Result<()> {
+        println!("Setting {} how upstream...", self.host_upstream);
+        if self.repo_path.is_empty() {
+            println!("It seems that you do not have cloned the repository locally");
+        }
+        let project = Repository::open(Path::new(&self.repo_path)).expect(NO_PROJECT_FOUND);
         project
-            .remote("upstream", self.upstream.url())
+            .remote("upstream", &self.url_upstream())
             .with_context(|| FAILED_TO_SET_REMOTE)?;
         Ok(())
     }
