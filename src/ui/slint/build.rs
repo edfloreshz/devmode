fc5789a1b26b39773613d4ae5f0f38380a74382d
Eduardo Flores <edfloreshz@gmail.com>
2024-01-16 19:02:42 -0700

    Global singletons object created

--- src/ui/slint/build.rs
+++ src/ui/slint/build.rs
@@ -1,3 +1,5 @@
 fn main() {
-    slint_build::compile("ui/appwindow.slint").unwrap();
+    let config = slint_build::CompilerConfiguration::new().with_style("fluent-dark".into());
+
+    slint_build::compile_with_config("ui/appwindow.slint", config).unwrap();
 }
--- src/ui/slint/src/main.rs
+++ src/ui/slint/src/main.rs
@@ -3,5 +3,9 @@ slint::include_modules!();
 fn main() -> Result<(), slint::PlatformError> {
     let ui = AppWindow::new()?;
 
+    ui.global::<Logic>().on_clone(move |url| {
+        println!("{url}");
+    });
+
     ui.run()
 }
