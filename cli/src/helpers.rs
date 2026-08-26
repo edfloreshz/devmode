b72116e11a20d5bc839c2662223a0c77ee640227
Eduardo Flores <edfloreshz@gmail.com>
2026-08-25 22:35:58 -0700

    chore: remove crates

--- cli/src/helpers.rs
+++
@@ -1,9 +0,0 @@
-use crate::log;
-
-// Helper to confirm the user wants to overwrite an existing repository.
-pub fn overwrite() -> bool {
-    log::warning("Found existing repository, overwrite it? y/n");
-    let mut input = String::new();
-    std::io::stdin().read_line(&mut input).unwrap();
-    matches!(input.trim(), "y" | "Y")
-}
